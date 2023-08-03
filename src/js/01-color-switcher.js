7ed60146ffafceee8e7b3219a26ecc6065fbeac4
wosean <serhii_v@ukr.net>
2023-08-03 22:54:37 +0300

    solution of the 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+document.querySelector('button[data-stop]').disabled = true;
+
+let timerId = 0;
+
+startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000,
+    document.querySelector('button[data-start]').disabled = true,
+    document.querySelector('button[data-stop]').disabled = false)
+});
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    document.querySelector('button[data-start]').disabled = false;
+    document.querySelector('button[data-stop]').disabled = true;
+});
