15cd1db9e5b6012e58722614c0150be11909e87f
wosean <serhii_v@ukr.net>
2023-08-08 02:06:02 +0300

    update # 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,9 @@
 import Notiflix from 'notiflix';
 
-const delay = document.querySelector('input[name="delay"]');
-const step = document.querySelector('input[name="step"]');
-const amount = document.querySelector('input[name="amount"]');
+let delay = document.querySelector('input[name="delay"]');
+let step = document.querySelector('input[name="step"]');
+let amount = document.querySelector('input[name="amount"]');
+
 const btnCreatePromise = document.querySelector('button[type="submit"]')
 
 function createPromise(position, delay) {
@@ -25,6 +26,12 @@ btnCreatePromise.addEventListener('click', e => {
   e.preventDefault();
   let firstDelay = Number(delay.value);
   let delayStep = Number(step.value);
+  if (firstDelay < 0 || delayStep < 0 || amount.value <= 0) {
+    Notiflix.Notify.info(
+      `Incorrect parametrs: ${delay}, ${step}ms, ${amount} `
+    );
+    return;
+      }  
   for (let i = 0; i < amount.value; i++) {
     createPromise(1 + i, firstDelay + i * delayStep)
       .then(({ position, delay }) => {
@@ -39,6 +46,52 @@ btnCreatePromise.addEventListener('click', e => {
       });
   }
 });
+
+
+
+
+
+// import Notiflix from 'notiflix';
+
+// const delay = document.querySelector('input[name="delay"]');
+// const step = document.querySelector('input[name="step"]');
+// const amount = document.querySelector('input[name="amount"]');
+// const btnCreatePromise = document.querySelector('button[type="submit"]')
+
+// function createPromise(position, delay) {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       const shouldResolve = Math.random() > 0.3;
+//       if (shouldResolve) {
+//         // Fulfill
+//         resolve({ position, delay });
+//       } else {
+//         // Reject
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+//   return promise;
+// }
+
+// btnCreatePromise.addEventListener('click', e => {
+//   e.preventDefault();
+//   let firstDelay = Number(delay.value);
+//   let delayStep = Number(step.value);
+//   for (let i = 0; i < amount.value; i++) {
+//     createPromise(1 + i, firstDelay + i * delayStep)
+//       .then(({ position, delay }) => {
+//         Notiflix.Notify.success(
+//           `✅ Fulfilled promise ${position} in ${delay}ms`
+//         );
+//       })
+//       .catch(({ position, delay }) => {
+//         Notiflix.Notify.failure(
+//           `❌ Rejected promise ${position} in ${delay}ms`
+//         );
+//       });
+//   }
+// });
 
     
       
